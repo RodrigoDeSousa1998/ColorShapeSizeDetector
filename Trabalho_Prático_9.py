28a15602bcb14e349d5288a7915d349091e1d6b4
Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>
2023-12-28 19:18:51 +0000

    feat: Output Image Added for UX
    Signed-off-by: Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>

--- Trabalho_Prático_9.py
+++ Trabalho_Prático_9.py
@@ -9,13 +9,13 @@
 
 ###################################### TO DO LIST ############################################
 
-#! TODO Meter contador de bolas de um  lado da imagem analisada real com circulos à volta
+#! TODO Meter contador de bolas de um  lado da imagem analisada com circulos à volta
 #! TODO Fazer deteções de linhas verdes e só contar circulos dentro das linhas verdes (ver se 
 #       há linha de meio campo, para contar ou só as bolas de uma lado, ou do outro)
-#! TODO Meter hierarchy para identificar apenas os contornos dos maior quadrado verde que encontrar
-#! TODO Fazer meno com teclas de comando para alternar a contagem de bolas de um lado e outro do campo 
-#! TODO Fazer videos com bolas a mexer-se dentro de campo no onenote (ou outra aplicação)
+#! TODO Fazer videos com bolas a mexer-se dentro de campo
 #! TODO Eliminar ruidos depois da calibração de cor, através de métricas de áreas da imagem
+#! TODO Meter hierarchy para identificar apenas os contornos dos maior quadrado verde que encontrar
+#! TODO Fazer menu com teclas de comando para alternar a contagem de bolas de um lado e outro do campo 
 #! TODO Fazer menu com teclas de comando para UX (para recablibrar, mudar raio de bolas etc)
 #! TODO Fazer funções para compactar código
 
@@ -33,6 +33,9 @@ MaxRadius  = 1
 MinRadius  = 1
 AccumThsld = 10
 
+# Variables Initial Values
+contours = None
+
 # Getting video feed from camera
 #videoFeed = cv2.VideoCapture(1)
 
@@ -45,17 +48,17 @@ def Map(x):
     temp=table.copy()
     cv2.rectangle(temp, (int(BallLT *6.42), 0), (int(BallUT *6.42), 140), (127, 127, 127), 2)
     cv2.rectangle(temp, (int(FieldLT*6.42), 0), (int(FieldUT*6.42), 140), (127, 127, 127), 2)
-    cv2.circle(temp, (380, 254), int(MinRadius*0.75), (255,0,0), 2)
-    cv2.circle(temp, (760, 254), int(MaxRadius*0.75), (255,0,0), 2)
+    cv2.circle(temp, (380, 254), int(MinRadius*0.5), (255,0,0), 2)
+    cv2.circle(temp, (760, 254), int(MaxRadius*0.5), (255,0,0), 2)
     cv2.imshow(window_name, temp)
 
 window_name = 'Calibrations'
 cv2.namedWindow(window_name)
-cv2.createTrackbar('Field LT',window_name,10,179, Map)
+cv2.createTrackbar('Field LT',window_name,1,179, Map)
 cv2.createTrackbar('Field UT',window_name,10,179, Map)
-cv2.createTrackbar('Ball LT',window_name,10,179, Map)
+cv2.createTrackbar('Ball LT',window_name,1,179, Map)
 cv2.createTrackbar('Ball UT',window_name,10,179, Map)
-cv2.createTrackbar('Min Radius',window_name,10,200, Map)
+cv2.createTrackbar('Min Radius',window_name,1,200, Map)
 cv2.createTrackbar('Max Radius',window_name,10,200, Map)
 cv2.createTrackbar('Acc.',window_name,10,200, Map)
 
@@ -64,6 +67,11 @@ cv2.createTrackbar('Acc.',window_name,10,200, Map)
 # while (ret and cv2.waitKey(1)!=27):
 while (cv2.waitKey(1)!=27):
     
+    # Working Image
+    if contours is None:
+        temp=image.copy()
+        cv2.imshow('Detected Circles',temp)
+
     # Conversion to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -80,14 +88,10 @@ while (cv2.waitKey(1)!=27):
     # Opening to remove noise from the feed
     opening = cv2.morphologyEx(FAD_filtered, cv2.MORPH_OPEN, kernel)
     # Closing to have clear edges
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-    FAD_denoised = closing
-
-    cv2.imshow('result', FAD_denoised)
+    FAD_denoised = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
     # Find contours in the binary mask
     contours, _ = cv2.findContours(FAD_denoised, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:5]
 
     # Iterate through the contours
     for contour in contours:
@@ -100,9 +104,9 @@ while (cv2.waitKey(1)!=27):
             # Draw a bounding box around the square
             x, y, w, h = cv2.boundingRect(contour)
 
-            # Extract the region inside the square
-            region_of_interest = image[y:y+h, x:x+w]
-            cv2.imshow('result2', region_of_interest)
+            # Extract the region inside the squares
+            region_of_interest = hsv[y:y+h, x:x+w]
+            blurred_ROI = cv2.medianBlur(region_of_interest, 9)
 
             # Ball Detection
             # Filter lower and upper bound
@@ -110,7 +114,7 @@ while (cv2.waitKey(1)!=27):
             BallUT = cv2.getTrackbarPos('Ball UT', window_name)
             BallMinColor = np.array([BallLT, 20, 20])
             BallMaxColor = np.array([BallUT, 255, 255])
-            BD_filtered = cv2.inRange(region_of_interest, BallMinColor, BallMaxColor)
+            BD_filtered = cv2.inRange(blurred_ROI, BallMinColor, BallMaxColor)
 
             MinRadius = cv2.getTrackbarPos('Min Radius', window_name)
             MaxRadius = cv2.getTrackbarPos('Max Radius', window_name)
@@ -119,15 +123,17 @@ while (cv2.waitKey(1)!=27):
             circles = cv2.HoughCircles(BD_filtered,cv2.HOUGH_GRADIENT,1,MinDistCircles, 
                                             param1=10,param2=AccumThsld,minRadius=MinRadius,maxRadius=MaxRadius)
 
+            temp=image.copy()[y:y+h, x:x+w]
             if circles is not None:
                 circles = np.uint16(np.around(circles))
                 for i in circles[0,:]:
-                    cv2.circle(BD_filtered,(i[0],i[1]),i[2],(255,0,0),2)
-                    cv2.circle(BD_filtered,(i[0],i[1]),2,(0,255,255),2)    
+                    cv2.circle(temp,(i[0],i[1]),i[2],(255,0,0),2)
+                    cv2.circle(temp,(i[0],i[1]),2,(0,255,255),2)    
                 number_circles = circles.shape[1]
                 print(number_circles)
-
-            cv2.imshow('detected circles',BD_filtered)
+            cv2.imshow('Detected Circles',temp)
+        else:
+            contours = None 
 
 #     ret, frame = videoFeed.read()
 # videoFeed.release()
