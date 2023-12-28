c25d0463f352666140ae4bf30b38f938923f5a23
Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>
2023-12-28 16:40:26 +0000

    feat: UI update
    Change name for trackbars
    Added trackbar for Accumulator Threshold
    Unified UI
    
    Signed-off-by: Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>

--- Trabalho_Prático_9.py
+++ Trabalho_Prático_9.py
@@ -23,10 +23,13 @@ import numpy as np
 import cv2
 
 # Calibrations Initial Values
-MaxH = 1
-MinH = 1
-MaxR = 1
-MinR = 1
+FieldUT    = 1
+FieldLT    = 1
+BallUT     = 1
+BallLT     = 1
+MaxRadius  = 1
+MinRadius  = 1
+AccumThsld = 10
 
 # Getting video feed from camera
 #videoFeed = cv2.VideoCapture(1)
@@ -34,32 +37,23 @@ MinR = 1
 imageSelected = cv2.imread("ImagemTeste1.png")
 
 # To create trackbar windows
-halfTable = cv2.imread("UI_Image1.png")
-fullTable = cv2.imread("UI_Image2.png")
+fullTable = cv2.imread("UI_Image.png")
 
-def fieldMap(x): 
-    temp=halfTable.copy()
-    cv2.rectangle(temp, (int(MinH*6.42), 0), (int(MaxH*6.42), 140), (127, 127, 127), 2)
-    cv2.imshow(window_name_1, temp)
-
-def ballMap(x): 
+def Map(x): 
     temp=fullTable.copy()
-    cv2.rectangle(temp, (int(MinH*6.42), 0), (int(MaxH*6.42), 140), (127, 127, 127), 2)
-    cv2.circle(temp, (569, 380), int(MinR), (255,0,0), 2)
-    cv2.circle(temp, (569, 760), int(MaxR), (255,0,0), 2)
-    cv2.imshow(window_name_2, temp)
+    cv2.rectangle(temp, (int(BallLT *6.42), 0), (int(BallUT *6.42), 140), (127, 127, 127), 2)
+    cv2.rectangle(temp, (int(FieldLT*6.42), 0), (int(FieldUT*6.42), 140), (127, 127, 127), 2)
+    cv2.circle(temp, (380, 254), int(MinRadius*0.75), (255,0,0), 2)
+    cv2.circle(temp, (760, 254), int(MaxRadius*0.75), (255,0,0), 2)
+    cv2.imshow('Calibrations', temp)
 
-window_name_1 = 'Field Line Calibrations'
-cv2.namedWindow(window_name_1)
-cv2.createTrackbar('MinH',window_name_1,10,179, fieldMap)
-cv2.createTrackbar('MaxH',window_name_1,10,179, fieldMap)
-
-window_name_2 = 'Ball Calibrations'
-cv2.namedWindow(window_name_2)
-cv2.createTrackbar('MinH',window_name_2,10,179, ballMap)
-cv2.createTrackbar('MaxH',window_name_2,10,179, ballMap)
-cv2.createTrackbar('MinR',window_name_2,10,200, ballMap)
-cv2.createTrackbar('MaxR',window_name_2,10,200, ballMap)
+cv2.createTrackbar('Field LT  ','Calibrations',10,179, Map)
+cv2.createTrackbar('Field UT  ','Calibrations',10,179, Map)
+cv2.createTrackbar('Ball LT   ','Calibrations',10,179, Map)
+cv2.createTrackbar('Ball UT   ','Calibrations',10,179, Map)
+cv2.createTrackbar('Min Radius','Calibrations',10,200, Map)
+cv2.createTrackbar('Max Radius','Calibrations',10,200, Map)
+cv2.createTrackbar('Acc.      ','Calibrations',10,200, Map)
 
 #Displaying the feed
 # ret, frame = videoFeed.read()
@@ -71,11 +65,11 @@ while (cv2.waitKey(1)!=27):
 
     # Field Area Detection
     # Filter lower and upper bound
-    MinH = cv2.getTrackbarPos('MinH', window_name_1)
-    MaxH = cv2.getTrackbarPos('MaxH', window_name_1)
-    min_color = np.array([MinH, 20, 20])
-    max_color = np.array([MaxH, 255, 255])
-    FAD_filtered = cv2.inRange(hsv, min_color, max_color)
+    FieldLT = cv2.getTrackbarPos('Field LT', 'Calibrations')
+    FieldUT = cv2.getTrackbarPos('Field UT', 'Calibrations')
+    FieldMinColor = np.array([FieldLT, 20, 20])
+    FieldMaxColor = np.array([FieldUT, 255, 255])
+    FAD_filtered = cv2.inRange(hsv, FieldMinColor, FieldMaxColor)
 
     # Define structuring element
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
@@ -106,17 +100,18 @@ while (cv2.waitKey(1)!=27):
             region_of_interest = imageSelected[y:y+h, x:x+w]
             cv2.imshow('result2', region_of_interest)
 
-            MinH = cv2.getTrackbarPos('MinH', window_name_2)
-            MaxH = cv2.getTrackbarPos('MaxH', window_name_2)
-            min_color = np.array([MinH, 20, 20])
-            max_color = np.array([MaxH, 255, 255])
-            BD_filtered = cv2.inRange(region_of_interest, min_color, max_color)
+            BallLT = cv2.getTrackbarPos('Ball LT', 'Calibrations')
+            BallUT = cv2.getTrackbarPos('Ball UT', 'Calibrations')
+            BallMinColor = np.array([BallLT, 20, 20])
+            BallMaxColor = np.array([BallUT, 255, 255])
+            BD_filtered = cv2.inRange(region_of_interest, BallMinColor, BallMaxColor)
 
-            MinR = cv2.getTrackbarPos('MinR', window_name_2)
-            MaxR = cv2.getTrackbarPos('MaxR', window_name_2)
+            MinRadius = cv2.getTrackbarPos('Min Radius', 'Calibrations')
+            MaxRadius = cv2.getTrackbarPos('Max Radius', 'Calibrations')
+            AccumThsld = cv2.getTrackbarPos('Acc.', 'Calibrations')
             MinDistCircles = 100
             circles = cv2.HoughCircles(BD_filtered,cv2.HOUGH_GRADIENT,1,MinDistCircles, 
-                                            param1=MinR,param2=MaxR,minRadius=0,maxRadius=0)
+                                            param1=10,param2=AccumThsld,minRadius=MinRadius,maxRadius=MaxRadius)
 
             if circles is not None:
                 circles = np.uint16(np.around(circles))
